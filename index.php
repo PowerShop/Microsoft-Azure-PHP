47285b78ae59dd17a4a77041be09cb8326e41201
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2022-05-02 04:03:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hello World
+<?php
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'jothanshop-server.mysql.database.azure.com', 'cnncpcbqme', 'Dekdee110#!!', 'mshop', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+} else {
+  echo 'Success!';
+}
+?>
